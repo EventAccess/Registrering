01644803bac5dfb06efc557542d5b5f1a4bc1b6c
Odd Stråbø <oddstr13@openshell.no>
2024-11-04 18:10:47 +0100

    Fix references frontend -> eventaccess_registration

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from frontend import views
+from eventaccess_registration import views
 
 
 urlpatterns = [
